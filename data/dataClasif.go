b9558a029e7401ac57195de3474ea2696b1a134e
naoufal dahouli <neufal777@gmail.com>
2021-06-10 00:49:16 +0200

    files manager

--- data/dataClasif.go
+++ data/dataClasif.go
@@ -1,7 +1,48 @@
 package data
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"github.com/Byron/core"
+	"github.com/ttacon/chalk"
+)
+
 /*
 	1- Process all the separated files
 	2- build only 1 file
 	3- delete duplicates
 */
+
+func FilesOrganizer(folder string) {
+	ArticlesProcessed := 0
+	filesProcessed := 0
+	AllArticles := []core.Article{}
+
+	files, _ := ioutil.ReadDir(folder)
+
+	for _, f := range files {
+		fmt.Println(chalk.Green.Color("Processing: " + f.Name()))
+
+		if strings.Contains(f.Name(), ".json") {
+			file := strings.Replace(f.Name(), ".json", "", -1)
+			articles := core.ReadArticles(file)
+
+			for _, art := range articles {
+				AllArticles = append(AllArticles, art)
+				ArticlesProcessed++
+			}
+		}
+		filesProcessed++
+	}
+
+	/*
+		Save all the articles in the same file
+	*/
+	core.WriteInFile("GENERAL", AllArticles)
+	log.Println("Total Articles:", ArticlesProcessed)
+	log.Println("Total Files:", filesProcessed)
+
+}
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"github.com/Byron/sources"
-)
+import "github.com/Byron/data"
 
 func main() {
 
@@ -10,28 +8,30 @@ func main() {
 	// flag.Parse()
 	// fmt.Println(chalk.Magenta.Color("processing " + *category))
 
-	excat := []string{"salsas", "botas"}
+	// excat := []string{"salsas", "botas"}
 
-	source := sources.Source{
-		SourceName:           "LIBGEN",
-		UrlREGEX:             "<a href=.book/index.php.md5=([^\"']*)",
-		IdREGEX:              "ID:</font></nobr></td><td>([^<]*)",
-		DownloadUrlREGEX:     "align=.center.><a href=.([^\"']*). title=.Gen.lib.rus.ec.",
-		TitleREGEX:           "<title>Library Genesis:([^<]*)",
-		IsbnREGEX:            "ISBN:</font></td><td>([^<]*)",
-		YearREGEX:            "Year:</font></nobr></td><td>([^<]*)",
-		PublisherREGEX:       "Publisher:</font></nobr></td><td>([^<]*)",
-		AuthorREGEX:          "Author.s.:</font></nobr></td><td colspan=3><b>([^<]*)",
-		ExtensionREGEX:       "Extension:</font></nobr></td><td>([^<]*)",
-		PageREGEX:            "Pages .biblio.tech.:</font></nobr></td><td>([^<]*)",
-		LanguageREGEX:        "Language:</font></nobr></td><td>([^<]*)",
-		SizeREGEX:            "Size:</font></nobr></td><td>([^<]*)",
-		TimeREGEX:            "Time modified:</font></nobr></td><td>([^<]*)",
-		CompletePageUrl:      "https://libgen.is/search.php?&res=100&req=" + excat[0] + "&phrase=1&view=simple&column=def&sort=year&sortmode=DESC&page=",
-		IncompleteArticleUrl: "https://libgen.is/book/index.php?md5=",
-		AllUrls:              nil,
-		Search:               excat[0],
-	}
+	// source := sources.Source{
+	// 	SourceName:           "LIBGEN",
+	// 	UrlREGEX:             "<a href=.book/index.php.md5=([^\"']*)",
+	// 	IdREGEX:              "ID:</font></nobr></td><td>([^<]*)",
+	// 	DownloadUrlREGEX:     "align=.center.><a href=.([^\"']*). title=.Gen.lib.rus.ec.",
+	// 	TitleREGEX:           "<title>Library Genesis:([^<]*)",
+	// 	IsbnREGEX:            "ISBN:</font></td><td>([^<]*)",
+	// 	YearREGEX:            "Year:</font></nobr></td><td>([^<]*)",
+	// 	PublisherREGEX:       "Publisher:</font></nobr></td><td>([^<]*)",
+	// 	AuthorREGEX:          "Author.s.:</font></nobr></td><td colspan=3><b>([^<]*)",
+	// 	ExtensionREGEX:       "Extension:</font></nobr></td><td>([^<]*)",
+	// 	PageREGEX:            "Pages .biblio.tech.:</font></nobr></td><td>([^<]*)",
+	// 	LanguageREGEX:        "Language:</font></nobr></td><td>([^<]*)",
+	// 	SizeREGEX:            "Size:</font></nobr></td><td>([^<]*)",
+	// 	TimeREGEX:            "Time modified:</font></nobr></td><td>([^<]*)",
+	// 	CompletePageUrl:      "https://libgen.is/search.php?&res=100&req=" + excat[0] + "&phrase=1&view=simple&column=def&sort=year&sortmode=DESC&page=",
+	// 	IncompleteArticleUrl: "https://libgen.is/book/index.php?md5=",
+	// 	AllUrls:              nil,
+	// 	Search:               excat[0],
+	// }
 
-	source.GetArticles()
+	// source.GetArticles()
+
+	data.FilesOrganizer("Inventory/")
 }
